e82cb11b709e0786a94e6a9f76b3d7ed382458bc
munozm01 <42704529+munozm01@users.noreply.github.com>
2018-08-26 01:40:20 +0200

    Update Program.cs
    added a comment to continue on and let others know what my intentions are

--- ConsoleApptest/Program.cs
+++ ConsoleApptest/Program.cs
@@ -12,6 +12,7 @@ namespace ConsoleApptest
             Console.WriteLine("this is the text to be printed: {0}", textToPrint.ToString());
             string SysInfo = System.DateTime.Today.ToString();
             Console.WriteLine("this is the current time: {0}", SysInfo);
+            //This are the building blocks for a clock feature
             for (int x=0; x < 10; x++) {
                 Thread.Sleep(1000);
                 Console.WriteLine("Count: {0}",x);
