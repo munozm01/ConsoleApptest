d41f550b6fb036b9c9eb31a57169e7a2a81a9db1
Manuel <test>
2018-08-26 00:39:32 +0200

    Added new feature

--- ConsoleApptest/Program.cs
+++ ConsoleApptest/Program.cs
@@ -9,6 +9,9 @@ namespace ConsoleApptest
             Console.WriteLine("Please write the text you want to be printed out");
             var textToPrint = Console.ReadLine();
             Console.WriteLine("this is the text to be printed: {0}", textToPrint.ToString());
+            string SysInfo = System.DateTime.Today.ToString();
+            Console.WriteLine("this is the current time: {0}", SysInfo);
+
         }
     }
 }
