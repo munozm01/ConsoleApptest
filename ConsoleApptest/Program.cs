691b9cd26475f67d391d73c7610f0439f7719d42
Manuel <test>
2018-08-26 00:47:25 +0200

    trying out something new

--- ConsoleApptest/Program.cs
+++ ConsoleApptest/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace ConsoleApptest
 {
@@ -11,7 +12,10 @@ namespace ConsoleApptest
             Console.WriteLine("this is the text to be printed: {0}", textToPrint.ToString());
             string SysInfo = System.DateTime.Today.ToString();
             Console.WriteLine("this is the current time: {0}", SysInfo);
-
+            for (int x=0; x < 10; x++) {
+                Thread.Sleep(1000);
+                Console.WriteLine("Count: {0}",x);
+            }
         }
     }
 }
