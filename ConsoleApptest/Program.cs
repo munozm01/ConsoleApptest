f2cd60d93ef6b33a4ae1ed29cb885309003e18a9
Manuel <test>
2018-08-26 00:33:31 +0200

    First running version of simple printing console app. Developed in VS for Mac. Testing version control over Git and VS for Mac

---
+++ ConsoleApptest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApptest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please write the text you want to be printed out");
+            var textToPrint = Console.ReadLine();
+            Console.WriteLine("this is the text to be printed: {0}", textToPrint.ToString());
+        }
+    }
+}
